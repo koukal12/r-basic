8a990d1eb3259c0208b57ec76436ae456690eaa6
Daniel Koukal <danielkoukal@MacBook-Air-de-Daniel.local>
2020-02-16 22:46:53 -0600

    Resumen

---
+++ Resumen Daniel .R
@@ -0,0 +1,22 @@
+#Resumen 
+help (lm) # función help  
+?sum
+help("+")
+help.start ()
+
+
+#Instalar Paquetes 
+install.packages(nombre del paquete), dep = TRUE)
+install.packages("tidyverse", dep = TRUE) 
+install.packages("magic", dep = TRUE)
+install.packages("abind", dep = TRUE)
+
+#Activar Paquetes 
+library(tidyverse)
+library(magic) #cuadrados magicos
+library(abind)
+
+#Ver Paquetes Instalados
+installed.packages()
+
+#Buscar paquetes en Google, por ejemplo magic tar gz , manual en PDF 
